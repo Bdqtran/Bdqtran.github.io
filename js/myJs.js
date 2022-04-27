14a3578b8ff7aada3dcff3240ac7757f7c7961da
Bdqtran <104504938+Bdqtran@users.noreply.github.com>
2022-04-27 19:52:50 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/wbwthluv";
+            window.location = "https://www.facebook.com/quetran.cherry";
           },
         });
       }
